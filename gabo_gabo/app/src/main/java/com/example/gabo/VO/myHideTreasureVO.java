ad9013cbfc84f4745060cc042ee66c2bdd5b664f
ojy <dsignshelter@naver.com>
2022-08-31 17:22:52 +0900

    ojy add vo

--- gabo_gabo/app/src/main/java/com/example/gabo/VO/myHideTreasureVO.java
+++ gabo_gabo/app/src/main/java/com/example/gabo/VO/myHideTreasureVO.java
@@ -1,5 +1,89 @@
 package com.example.gabo.VO;
 
-public class myHideTreasureVO {
+import android.graphics.drawable.Drawable;
 
+public class myHideTreasureVO {
+    private Drawable myhide_img_profile;
+    private Drawable myhide_img_like;
+    private String myhide_tv_km;
+    private String myhide_tv_tag1;
+    private String myhide_tv_tag2;
+    private String myhide_tv_tag3;
+    private String myhide_tv_when;
+    private String myhide_tv_like;
+
+    public myHideTreasureVO(Drawable myhide_img_profile, Drawable myhide_img_like, String myhide_tv_km, String myhide_tv_tag1, String myhide_tv_tag2, String myhide_tv_tag3, String myhide_tv_when, String myhide_tv_like) {
+        this.myhide_img_profile = myhide_img_profile;
+        this.myhide_img_like = myhide_img_like;
+        this.myhide_tv_km = myhide_tv_km;
+        this.myhide_tv_tag1 = myhide_tv_tag1;
+        this.myhide_tv_tag2 = myhide_tv_tag2;
+        this.myhide_tv_tag3 = myhide_tv_tag3;
+        this.myhide_tv_when = myhide_tv_when;
+        this.myhide_tv_like = myhide_tv_like;
+    }
+
+    public Drawable getMyhide_img_profile() {
+        return myhide_img_profile;
+    }
+
+    public void setMyhide_img_profile(Drawable myhide_img_profile) {
+        this.myhide_img_profile = myhide_img_profile;
+    }
+
+    public Drawable getMyhide_img_like() {
+        return myhide_img_like;
+    }
+
+    public void setMyhide_img_like(Drawable myhide_img_like) {
+        this.myhide_img_like = myhide_img_like;
+    }
+
+    public String getMyhide_tv_km() {
+        return myhide_tv_km;
+    }
+
+    public void setMyhide_tv_km(String myhide_tv_km) {
+        this.myhide_tv_km = myhide_tv_km;
+    }
+
+    public String getMyhide_tv_tag1() {
+        return myhide_tv_tag1;
+    }
+
+    public void setMyhide_tv_tag1(String myhide_tv_tag1) {
+        this.myhide_tv_tag1 = myhide_tv_tag1;
+    }
+
+    public String getMyhide_tv_tag2() {
+        return myhide_tv_tag2;
+    }
+
+    public void setMyhide_tv_tag2(String myhide_tv_tag2) {
+        this.myhide_tv_tag2 = myhide_tv_tag2;
+    }
+
+    public String getMyhide_tv_tag3() {
+        return myhide_tv_tag3;
+    }
+
+    public void setMyhide_tv_tag3(String myhide_tv_tag3) {
+        this.myhide_tv_tag3 = myhide_tv_tag3;
+    }
+
+    public String getMyhide_tv_when() {
+        return myhide_tv_when;
+    }
+
+    public void setMyhide_tv_when(String myhide_tv_when) {
+        this.myhide_tv_when = myhide_tv_when;
+    }
+
+    public String getMyhide_tv_like() {
+        return myhide_tv_like;
+    }
+
+    public void setMyhide_tv_like(String myhide_tv_like) {
+        this.myhide_tv_like = myhide_tv_like;
+    }
 }

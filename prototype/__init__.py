1bbf103b50490597e43c655c198735f8c28b6bfb
ljy <eejyng94@gmail.com>
2022-09-01 23:33:41 +0900

    s

--- prototype/__init__.py
+++ prototype/__init__.py
@@ -265,5 +265,5 @@ def mainPage():
     return render_template('index.html')   
           
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5013))
+    port = int(os.environ.get("PORT", "5000"))
     app.run(host='0.0.0.0', port=port)

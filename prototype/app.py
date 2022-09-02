5e3826cd6b688cc874fac56fbc56f38d48e812ea
ljy <eejyng94@gmail.com>
2022-09-02 09:05:10 +0900

    s

--- prototype/app.py
+++ prototype/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for, session
 import cx_Oracle as db
-
+import os
 
 app = Flask(__name__)
 
@@ -263,4 +263,7 @@ def joinAdmin():
 @app.route('/mainPage', methods = ['GET'])
 def mainPage():
     return render_template('index.html')   
-          
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
